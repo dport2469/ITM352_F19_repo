7ba69fcbb29d8f888695f40f8f59703709cb7caf
dport9696 <54292872+dport9696@users.noreply.github.com>
2019-10-15 11:45:59 -1000

    xx

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,6 +1,21 @@
+/*
 var age = 22;
 
 for(var number = 50; number >= age; number--){
     console.log(`Are you ${number} years old?`);
 }
 console.log(`Are you ${number} years old?`);
+
+a = [1,2,3];
+*/
+
+product1 = { 'name': 'small gumball', 'price': 0.02 };
+product2 = { 'name': 'medium gumball', 'price': 0.05 };
+product3 = { 'name': 'large gumball', 'price': 0.07 };
+
+// array of all products
+products = [product1, product2, product3];
+cart_quantities = [2,0,4]; // corlsresponds to products array
+for(i=0; i<products.length; i++) {
+    console.log(`extnded price for product ${products[i].name} is ${products[i].price * cart_quantities[i]}`);
+}
