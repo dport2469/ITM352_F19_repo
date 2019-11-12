e02cce5be26d33c82a7cbe6491118283b14d7394
dport9696 <54292872+dport9696@users.noreply.github.com>
2019-11-12 11:21:25 -1000

    xx

--- Lab13/info_server_Ex3.js
+++ Lab13/info_server_Ex3.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var myParser = require("body-parser");
+var fs = require('fs');
 
 var app = express();
 app.all('*', function (request, response, next) {
@@ -28,9 +29,13 @@ function isNonNegInt(q, returnErrors = false) {
 
 function displayPurchase(POST, response) {
     q = POST['quantity_textbox'];
-    if (isNonNegInt(q)) {
-        response.send(`Thank you for purchasing ${q} things!`);
-    } else {
-        response.send(`${q} is not a quantity! Press the back button and try again.`);
+    if (typeof POST['quantity_textbox'] != 'undefined') {
+        let q = POST['quantity_textbox'];
+        if (isNonNegInt(q)) {
+            var contents = fs.readFileSync('./views/display_quantity_template.view', 'utf8');
+            response.send(eval('`' + contents + '`')); // render template string
+        } else {
+            response.send(`${q} is not a quantity!`);
+        }
     }
 }
